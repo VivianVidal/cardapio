8dca7d6e130f3f6d16e2b5a57b12274f6f59e8d8
Vivian <viviancristinavidal@gmail.com>
2024-09-22 20:17:20 -0300

    Feat: adicionando estrutura  componente Topo

---
+++ src/app/components/Topo/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Topo(){
+    return(
+        <header>
+            <div>
+                <h1>Restaurant</h1>
+                <p>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados.</p>
+            </div>
+        </header>
+    )
+}
