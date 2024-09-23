b7d6aea25f3a6db9d71192fdb5992a05f37b2f2e
Vivian <viviancristinavidal@gmail.com>
2024-09-22 21:37:09 -0300

    Style: aplicando estilos do componente Topo e refatorando layout para uso de variaveis das fontes

--- src/app/components/Topo/index.tsx
+++ src/app/components/Topo/index.tsx
@@ -1,10 +1,12 @@
+import styles from './Topo.module.css';
+
 
 export default function Topo(){
     return(
-        <header>
-            <div>
-                <h1>Restaurant</h1>
-                <p>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados.</p>
+        <header className={styles.header_container}>
+            <div className={styles.banner}>
+                <h1 className={styles.title}>Restaurant</h1>
+                <p className={styles.text}>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados.</p>
             </div>
         </header>
     )
